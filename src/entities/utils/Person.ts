bec3716115e01e6c42caf112bca2140ac9272702
Bassit <barseetbrown@gmail.com>
2023-08-14 16:26:01 +0100

    feat: create client and banker

--- src/entities/utils/Person.ts
+++ src/entities/utils/Person.ts
@@ -23,12 +23,4 @@ export class Person extends BaseEntity {
         length: 10
     })
     card_number: string;
-
-    @Column({
-        type: 'numeric'
-    })
-    balance: number
-
-
-
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { createConnection } from "typeorm"
+import express from "express"
 import { Client } from "./entities/Client"
 import { Banker } from "./entities/Banker"
 import { Transaction } from "./entities/Transaction"
+import { createClientRouter } from "./routes/create_clients"
+import { createBankerRouter } from "./routes/create_banker"
 
 require('dotenv').config()
 
+const app = express()
 
 const main = async () => {
     try {
@@ -19,6 +23,13 @@ const main = async () => {
             synchronize: true,
         })
         console.log('Database connected')
+
+        app.use(express.json())
+        app.use(createClientRouter)
+        app.use(createBankerRouter)
+        app.listen(8080, () => {
+            console.log('Server started on port 8080')
+        })
     } catch (error) {
         console.error(error)
         throw new Error('Unable to connect to database')
